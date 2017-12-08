52025109587904ec4355c1e1976c3341a09ac20e
Itamar <doriitamar@gmail.com>
2017-12-08 22:20:34 +0200

    Made an iframe that reloads itself when there's no sheilta tab to refresh

--- background.js
+++ background.js
@@ -2,32 +2,37 @@
 chrome.alarms.create("refreshNow", {"periodInMinutes": 15.0});
 
 let lastAction = Date.now();
+let ifrm = document.createElement('iframe');
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     let url = changeInfo.url ? changeInfo.url : tab.url;
 
-    if(url.indexOf("openu.ac.il") != -1) {
+    if(url.indexOf("sheilta") != -1) {
         lastAction = Date.now();
     }
 })
 
 chrome.alarms.onAlarm.addListener((alarm) => {
-  if(alarm.name === "refreshNow") {
+  if(alarm.name === "refreshNow" && navigator.onLine) {
       chrome.tabs.query({"url": "https://*.openu.ac.il/*"}, (tabs) => {
           let chosenTab;
 
           // Preventing refresh while watching videos
           for(let currTab of tabs) {
-                if(currTab.url.indexOf('video') === -1) {
+                if(currTab.url.indexOf('sheilta') !== -1) {
                     chosenTab = currTab;
                     break;
                 }
           }
-          
+
           // Refreshing only if the last interaction with openu website was
           // at least 10 minutes ago and there's a tab to refresh
-          if(chosenTab && (lastAction + 600000) <= Date.now()) {
-              chrome.tabs.reload(chosenTab.id);
+          if((lastAction + 600000) <= Date.now()) {
+              if(chosenTab) {
+                  chrome.tabs.reload(chosenTab.id);
+              } else { // If there isn't a sheilta tab active
+                 window.location.reload()
+              }
           }
       })
   }
