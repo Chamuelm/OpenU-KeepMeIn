5cd829c4eea42d30332c5df59fb8670ef1d8a4de
Itamar <doriitamar@gmail.com>
2017-12-09 16:14:14 +0200

    fixed what I tried to fix on my last commit :')

--- background.js
+++ background.js
@@ -1,23 +1,23 @@
 // Using alarms so an event will be fired and this won't be a persistent script
 chrome.alarms.create("refreshNow", {"periodInMinutes": 15.0});
 
-let lastAction = Date.now();
-let ifrm = document.createElement('iframe');
+let lastAction = -1;
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     let url = changeInfo.url ? changeInfo.url : tab.url;
 
-    if(url.indexOf("sheilta") != -1) {
+    // Capturing login/last interaction with sheilta
+    if(url.indexOf("sheilta") !== -1) {
         lastAction = Date.now();
     }
 })
 
 chrome.alarms.onAlarm.addListener((alarm) => {
-  if(alarm.name === "refreshNow" && navigator.onLine) {
+  if(alarm.name === "refreshNow" && navigator.onLine && lastAction !== -1) {
       chrome.tabs.query({"url": "https://*.openu.ac.il/*"}, (tabs) => {
           let chosenTab;
 
-          // Preventing refresh while watching videos
+          // Searching for a sheilta page
           for(let currTab of tabs) {
                 if(currTab.url.indexOf('sheilta') !== -1) {
                     chosenTab = currTab;
@@ -28,9 +28,10 @@ chrome.alarms.onAlarm.addListener((alarm) => {
           // Refreshing only if the last interaction with openu website was
           // at least 10 minutes ago and there's a tab to refresh
           if(chosenTab && (lastAction + 600000) <= Date.now()) {
-                  chrome.tabs.reload(chosenTab.id);
-          } else if(tabs && tabs.length > 0) { // If there isn't a sheilta tab active but there's an openu one
-              window.location.reload()
+              chrome.tabs.reload(chosenTab.id);
+          } else if(tabs && tabs.length > 0 && (lastAction + 600000) <= Date.now()) {
+              // If there isn't a sheilta tab active but there's an openu one
+              document.getElementById('refresh').src = "https://sheilta.apps.openu.ac.il/pls/dmyopt2/KOLHAODAOT.showhod";
           }
       })
   }
