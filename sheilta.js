923f77e4ad271da633e264ff834e7b2b0f70fd48
Moshe Hamiel <chamuelm@gmail.com>
2019-08-20 16:25:46 +0300

    Some selector and DOM changes

--- sheilta.js
+++ sheilta.js
@@ -4,13 +4,13 @@ const parser = new DOMParser();
 const selectors = {
   mainPage__messages: '#right_col > div:nth-child(3) > table > tbody > tr > td > form > table > tbody > tr:nth-child(2) > td > table > tbody > tr > td > table > tbody > tr:nth-child(3)',
   mainPage_messages_actions: '#right_col > div:nth-child(4) > table > tbody > tr > td > form > table > tbody > tr:nth-child(1) > td > div:nth-child(2)',
-  mainPage_grades_header: '#right_col > div:nth-child(3) > table > tbody > tr > td > form > table > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(1) > td:nth-child(1)',
-  mainPage_requests_actions: '#right_col > div:nth-child(4) > table > tbody > tr > td > form > table > tbody > tr:nth-child(6) > td > table:nth-child(2) > tbody > tr:nth-child(1) > td > div:nth-child(2)',
-  mainPage_requests_actions_fallback: '#right_col > div:nth-child(3) > table > tbody > tr:nth-child(2) > td > table > tbody > tr > td > div:nth-child(2)',
+  mainPage_grades_header: '#right_col > div:nth-child(4) > table > tbody > tr:nth-child(1) > td > form > table > tbody > tr:nth-child(3) > td > table > tbody > tr:nth-child(1)',
+  mainPage_requests_actions: '#right_col > div:nth-child(4) > table > tbody > tr:nth-child(2) > td > table > tbody > tr > td > div:nth-child(2)',
   s360_avg_grade: '#body > section > div > div > div.col-md-10.col-md-offset-1.main-wrapper > div.row.info.green-info > div > div:nth-child(2) > div.row.space-bottom > div:nth-child(3) > p',
   s360_classes: '#body > section > div > div > div.col-md-10.col-md-offset-1.main-wrapper > div.row.info.green-info > div > div:nth-child(4)',
   mainPage_sideCard: '#left_col > div:nth-child(1)',
-  mainPage_newTable: '#right_col > div:nth-child(4) > table > tbody > tr > td > form',
+  // mainPage_newTable: '#right_col > div:nth-child(4) > table > tbody > tr > td > form',
+  mainPage_newTable: '#right_col > div:nth-child(4) > table > tbody > tr:nth-child(2)',
   s360_points: '#body > section > div > div > div.col-md-10.col-md-offset-1.main-wrapper > div.row.info.green-info > div > div:nth-child(2) > div.row.space-bottom > div:nth-child(2) > p:nth-child(3)',
   s360_pending_points: '#body > section > div > div > div.col-md-10.col-md-offset-1.main-wrapper > div.row.info.green-info > div > div:nth-child(2) > div.row.space-bottom > div:nth-child(2) > p:nth-child(4)',
   s360_number_of_couses: '#body > section > div > div > div.col-md-10.col-md-offset-1.main-wrapper > div.row.info.light-green-info > div > div:nth-child(2) > div.row > div:nth-child(1) > div > div:nth-child(2) > p > span'
@@ -56,12 +56,16 @@ function mainPage() {
   try {
     if(window.top.document.querySelector(selectors.mainPage_grades_header)) {
       window.top.document.querySelector(selectors.mainPage_grades_header).setAttribute('colspan', 5);
-      const grdButton = parser.parseFromString(`<td colspan="2" style="border: 0px;" nowrap="" bgcolor="#ffffff" align="left">
-      <div class="message_actions">
+      const grdButton = document.createElement("td");
+      grdButton.setAttribute("colspan", "2");
+      grdButton.setAttribute("style", "border: 0px;");
+      grdButton.setAttribute("nowrap", "");
+      grdButton.setAttribute("bgcolor", "#ffffff");
+      grdButton.setAttribute("align", "left");
+      grdButton.append(parser.parseFromString(`<div class="message_actions">
       <a class="niceButton" id="selectAllGrades">בטל סימון להכל</a>
-      </div>
-      </td>`, "text/html");
-      window.top.document.querySelector(selectors.mainPage_grades_header).after(grdButton.body.firstChild);
+      </div>`, "text/html").body.firstChild);
+      window.top.document.querySelector(selectors.mainPage_grades_header).append(grdButton);
     
       realDoc.getElementById('selectAllGrades').addEventListener('click', selectAllGrades);
     }
@@ -77,9 +81,11 @@ function mainPage() {
     <a class="niceButton" href="javascript:submit_all('pniya')">עדכון תצוגה</a>
     <a class="niceButton" href="kolhapniot.newsearch">כל הבקשות</a>
     </div>`, "text/html");
-    const requests =   window.top.document.querySelector(selectors.mainPage_requests_actions) || window.top.document.querySelector(selectors.mainPage_requests_actions_fallback)
-    requests.replaceWith(reqButton.body.firstChild)
-    realDoc.getElementById('selectAllRequests').addEventListener('click', selectAllRequests);
+    const requests =   window.top.document.querySelector(selectors.mainPage_requests_actions)
+    if (requests) {
+      requests.replaceWith(reqButton.body.firstChild);
+      realDoc.getElementById('selectAllRequests').addEventListener('click', selectAllRequests);
+    }
   } catch(err) {
     console.warn(err);
   }
